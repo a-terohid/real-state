abb603d459c276f6ef80c65080dbae2b85a1a322
a-terohid <87315139+a-terohid@users.noreply.github.com>
2023-08-30 23:02:25 +0330

    fixed logout buttom error

--- src/components/module/LogOutButtom.tsx
+++ src/components/module/LogOutButtom.tsx
@@ -4,7 +4,7 @@ import { signOut } from "next-auth/react";
 import { FiLogOut } from "react-icons/fi";
 
 const LogOutButtom = () => {
-    return ( <button className=' bg-[#ff8fa3] text-[#a4133c] font-bold md:px-8 flex gap-x-2 items-center  px-3 py-1 rounded-md ' onClick={signOut}>LogOut<FiLogOut /></button> );
+    return ( <button className=' bg-[#ff8fa3] text-[#a4133c] font-bold md:px-8 flex gap-x-2 items-center  px-3 py-1 rounded-md ' onClick={ () => signOut() }>LogOut<FiLogOut /></button> );
 };
 
 export default LogOutButtom;
